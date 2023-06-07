e538dd76d3d49e2959a651540c90ff1088109330
Usman Liaqat <memerboyemiology444@gmail.com>
2023-06-07 15:27:20 +0500

    first commit

---
+++ .next/static/webpack/pages/_app.3aeea3cb255a92b0.hot-update.js
@@ -0,0 +1,32 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/_app",{
+
+/***/ "./public/images/bg-01.jpg":
+/*!*********************************!*\
+  !*** ./public/images/bg-01.jpg ***!
+  \*********************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/media/bg-01.e08b3b72.jpg";
+
+/***/ }),
+
+/***/ "./public/images/bg-03.jpg":
+/*!*********************************!*\
+  !*** ./public/images/bg-03.jpg ***!
+  \*********************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "static/media/bg-03.6cef7ad1.jpg";
+
+/***/ })
+
+});
